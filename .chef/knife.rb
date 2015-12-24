b90325f89001cc4b728901e8420fb8c3cb9d34c0
Rong Liang <rong.liang@hpe.com>
2015-12-24 04:40:47 -0500

    initial commit

---
+++ .chef/knife.rb
@@ -0,0 +1,15 @@
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "rlneo"
+client_key               "#{current_dir}/rlneo.pem"
+validation_client_name   "hpneo-validator"
+validation_key           "#{current_dir}/hpneo-validator.pem"
+chef_server_url          "https://api.chef.io/organizations/hpneo"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+http_proxy               "http://web-proxy.jpn.hp.com:8080"
+https_proxy              "http://web-proxy.jpn.hp.com:8080"
+require 'rest-client'
+RestClient.proxy = "http://web-proxy.jpn.hp.com:8080"
